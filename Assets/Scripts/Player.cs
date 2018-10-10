ed0b2714b8fced6f6366d85b436b36aa2106567e
ernest68680 <ernest.dlugoszewski@gmail.com>
2018-10-10 11:33:50 +0100

    Saving Scripts
    Making a backup copy of my health bar scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    public float jumpSpeed = 20;
+
+    [SerializeField]
+    private Stat health;
+
+    [SerializeField]
+    private Stat armor;
+
+    private void Awake()
+    {
+        health.Initialize();
+        armor.Initialize();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (armor.CurrentVal <= 0)
+            {
+                health.CurrentVal -= 10;
+            }
+            else
+            {
+                armor.CurrentVal -= 20;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (health.CurrentVal >= 100)
+            {
+                armor.CurrentVal += 20;
+            }
+            else
+            {
+                health.CurrentVal += 10;
+            }
+
+        }
+
+        float Dirx = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.position = new Vector2(transform.position.x + Dirx, transform.position.y);
+
+        if (Input.GetKeyDown("space"))
+        {
+            transform.Translate(Vector3.up * 100 * Time.deltaTime, Space.World);
+        }
+    }
+}
+
